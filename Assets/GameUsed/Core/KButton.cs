0c2cba2933675fb0660a15f986df2349ff2fbbed
Ponito <vvbdac58@gmail.com>
2024-12-14 16:51:05 +0800

    目前寫到可以用按鈕移動夾子

---
+++ Assets/GameUsed/Core/KButton.cs
@@ -0,0 +1,50 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace GameUsed.Core
+{
+    // 為了讓在測試時可以用滑鼠互動，又能讓Kinect互動，製作的按鈕
+    public class KButton :
+        MonoBehaviour,
+        IPointerDownHandler, IPointerUpHandler
+    {
+        [Tooltip("是否重複執行 OnClick")]
+        [SerializeField] public bool       isRepeatedly;
+
+        [Tooltip("按下時執行的事件")]
+        [SerializeField] public UnityEvent onClick;
+
+        private CancellationTokenSource cts { get; set; } = new();
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            cts?.Cancel();
+            cts = new CancellationTokenSource();
+            if (isRepeatedly) Loop().Forget();
+            else onClick?.Invoke();
+        }
+
+        private async UniTask Loop()
+        {
+            while (!cts.IsCancellationRequested)
+            {
+                await UniTask.WaitForFixedUpdate();
+                onClick?.Invoke();
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            cts?.Cancel();
+        }
+
+        private void OnDestroy()
+        {
+            cts?.Cancel();
+            cts?.Dispose();
+        }
+    }
+}
--- Assets/GameUsed/Scenes/Title/Claw.cs
+++ Assets/GameUsed/Scenes/Title/Claw.cs
@@ -1,4 +1,6 @@
+using System;
 using Cysharp.Threading.Tasks;
+using GameUsed.Core;
 using UnityEngine;
 
 namespace GameUsed.Scenes.Title
@@ -6,17 +8,39 @@ namespace GameUsed.Scenes.Title
     [AddComponentMenu("Title/Claw")]
     public class Claw : MonoBehaviour
     {
+        private const float MAX_X = +0.9f;
+        private const float MIN_X = -0.9f;
+
+        [SerializeField] private float     speed;
         [SerializeField] private Transform clawL;
         [SerializeField] private Transform clawR;
+        [SerializeField] private KButton   left;
+        [SerializeField] private KButton   right;
+        [SerializeField] private KButton   grab;
+
+        private Rigidbody self { get; set; }
+
+        private void OnEnable()
+        {
+            self = GetComponent<Rigidbody>();
+            // 左右對於攝影機中的世界是顛倒的
+            left.onClick.AddListener(() => Shift(+speed));
+            right.onClick.AddListener(() => Shift(-speed));
+        }
+
+        private void Shift(float speed)
+        {
+            var position = self.position;
+            position.x = Mathf.Clamp(position.x + speed * Time.deltaTime, MIN_X, MAX_X);
+            self.MovePosition(position);
+        }
 
         public async UniTask Grab()
         {
-
         }
 
         public async UniTask Release()
         {
-
         }
     }
 }
